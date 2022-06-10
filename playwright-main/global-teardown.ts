97ba2f837b709da8f9847b98027ae54b0364287a
Elena Yasinskaya <e.yasinskaya@sana-commerce.com>
2022-06-10 10:49:36 +0300

    removed files

--- playwright-main/global-teardown.ts
+++
@@ -1,6 +0,0 @@
-//global-teardown.ts
-const exec = require('child_process').execSync;
-async function globalSetup() {
-    await exec('npx xunit-viewer -r  results.xml -o results.html ')
-  };
-export default globalSetup;
